4be96dc4ae9e6dc5d3b33b8121f10dc68afd6def
adam-weiler <adam.weiler@yahoo.ca>
2019-07-15 17:07:58 -0400

    All functions are working correctly.

--- paperboy.py
+++ paperboy.py
@@ -10,71 +10,58 @@ class PaperBoy():
         return f'PaperBoy instance:name={self.name} experience={self.experience} earnings={self.earnings}'
 
     def quota(self):
-        # self.quota = quota
         quota = 50 + (self.experience / 2)
         return quota
-         #Minimum: At least 50 papers
-        #Quota = Experience / 2 + Minimum
 
     def deliver(self, start_address, end_address):
         quota = self.quota()
 
-        self.experience += (end_address - start_address)
+        current_route = end_address - start_address
 
-        # self.earnings += self.experience
+        self.experience += (current_route)
 
-
-        if (self.experience < quota):
-            print('not enoguh!')
-            print(self.experience)
-            print(quota)
-            pass
+        if (current_route < quota):
+            print('Not Enough!')
+            # print(self.experience)
+            # print(quota)
+            #-2.0 if they didn't meet quota.
+            current_earnings = (current_route  * .25) - 2
+            # print(self.earnings)
         else:
-            self.earnings += ((self.experience - quota) * .5) + (quota * .25)
-
-
-            
-            
-            
-            
-            print('Thats good!')
-            print(self.experience)
-            print(quota)
-            # self.earnings += self.experience * .25
-
-
-            
-
-
-
+            print('You sold enough papers!')
+            # print(self.experience)
+            # print(quota)
+            current_earnings = ((current_route - quota) * .5) + (quota * .25)
         
-        return self.earnings
+        self.earnings += current_earnings
 
-
+        return current_earnings
 
     def report(self):
-        pass
-
-        #.25 for each paper
-        #.50 for each paper over quota
-        #-2.0 if they didn't meet quota.
-       
-
-        #Run 1, quota = 50
-        #Run 2, quota = 50 + (.5 * papers delivered before)
+        return f"I'm {self.name}, I've delivered {self.experience} papers and I've earned ${self.earnings} so far!"
 
 
-# johnny = PaperBoy('Johnny', 0, 0)
+tommy = PaperBoy('Tommy')
+
+print(tommy.quota()) #50
+print(tommy.deliver(100, 160)) #17.5
+print(tommy.earnings) #17.5
+print(tommy.report())
+print()
+
+print(tommy.quota()) #80
+print(tommy.deliver(1, 76)) #16.75
+print(tommy.earnings) #34.25
+print(tommy.report()) #"I'm Tommy, I've been delivered 135 papers and I've earned $34.25 so far!"
+print()
+
+print(tommy)
+
+
+
+# johnny = PaperBoy('Johnny')
 # print(johnny.quota())
 # print(johnny.quota())
 # print(johnny.deliver(42, 45))
 
 # print(johnny)
-
-
-
-tommy = PaperBoy('Tommy')
-print(tommy.quota()) #50
-print(tommy.deliver(100, 160)) #17.5
-
-print(tommy)
