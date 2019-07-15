ae9a8ff570ff8161f2595c1d3c1192d6bf4ac2ac
adam-weiler <adam.weiler@yahoo.ca>
2019-07-15 16:17:00 -0400

    Adding quota ethod

--- paperboy.py
+++ paperboy.py
@@ -1,14 +1,41 @@
 class PaperBoy():
 	#name, experience, earnings
 
+    def __init__(self, name, experience, earnings):
+        self.name = name
+        self.experience = experience
+        self.earnings = earnings
+
     def __str__(self):
-        pass
+        return f'PaperBoy instance:name={self.name} experience={self.experience} earnings={self.earnings}'
 
     def quota(self):
-        pass
+        # self.quota = quota
+        quota = 50 + (self.experience / 2)
+        return quota
+         #Minimum: At least 50 papers
+        #Quota = Experience / 2 + Minimum
 
     def deliver(self, start_address, end_address):
-        pass
+        quota = self.quota()
+
+
+
+        self.earnings += (end_address - start_address) 
+        
+        return earnings
 
     def report(self):
         pass
+
+        #.25 for each paper
+        #.50 for each paper over quota
+        #-2.0 if they didn't meet quota.
+       
+
+        #Run 1, quota = 50
+        #Run 2, quota = 50 + (.5 * papers delivered before)
+
+
+johnny = PaperBoy('Johnny', 0, 0)
+print(johnny)
