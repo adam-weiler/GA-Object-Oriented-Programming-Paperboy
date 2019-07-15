d27b2202a1e5e10ad8d4b26568f119449ace27b9
adam-weiler <adam.weiler@yahoo.ca>
2019-07-15 16:05:41 -0400

    Creating skeleton of app.

---
+++ paperboy.py
@@ -0,0 +1,14 @@
+class PaperBoy():
+	#name, experience, earnings
+
+    def __str__(self):
+        pass
+
+    def quota(self):
+        pass
+
+    def deliver(self, start_address, end_address):
+        pass
+
+    def report(self):
+        pass
